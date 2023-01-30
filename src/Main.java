64a0283eed719cb8a4673b115181eaa4ffc95603
Viacheslav Privalov <privalov2306@yandex.ru>
2023-01-30 12:43:43 +0300

    Made 1-3 tasks

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,52 @@ public class Main {
         task7();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача1");
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, " +
+                    "нужно немного подождать");
+        }
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача2");
+        int temp = 4;
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
+        }
     }
 
-    public static void task3 () {
+    public static void task3() {
         System.out.println("Задача3");
-    }
+        int speed = 50;
+        if (speed >= 60) {
+            System.out.println("Если скорость "+speed+", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость "+speed+", то можно ездить спокойно");
+        }
+        }
 
-    public static void task4 () {
-        System.out.println("Задача4");
-    }
+        public static void task4 () {
+            System.out.println("Задача4");
+        }
 
-    public static void task5 () {
-        System.out.println("Задача5");
-    }
+        public static void task5 () {
+            System.out.println("Задача5");
+        }
 
-    public static void task6 () {
-        System.out.println("Задача6");
-    }
+        public static void task6 () {
+            System.out.println("Задача6");
+        }
 
-    public static void task7 () {
-        System.out.println("Задача7");
-    }
+        public static void task7 () {
+            System.out.println("Задача7");
+        }
 
-}
+    }
